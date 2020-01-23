8319b5c75f5358915a8f5efc47868991ec0a225a
raberin <rmvaberin@gmail.com>
2020-01-22 21:14:36 -0800

    Added bottom down approach for eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,7 +7,7 @@ import sys
 # recursive solution
 
 
-# Naive Recursive Implementation O(c^n)
+# Naive Recursive Implementation O(3^n)
 def eating_cookies_naive(n, cache=None):
     # Base cases
     if n < 3:
@@ -18,7 +18,7 @@ def eating_cookies_naive(n, cache=None):
     return eating_cookies_naive(n-1) + eating_cookies_naive(n-2) + eating_cookies_naive(n-3)
 
 
-print(eating_cookies_naive(30))
+# print(eating_cookies_naive(30))
 
 
 # Memoized/Cached Recursive Implementation O(n^2)
@@ -31,7 +31,32 @@ def eating_cookies(n, cache={0: 1, 1: 1, 2: 2, 3: 4}):
         return cache[n]
 
 
-print(eating_cookies(30))
+print(eating_cookies(5))
+
+
+# Iterative Bottom-Down Approach O(n) -- Needs work/refinement
+def eating_cookies_iterative(n):
+    # Base Case
+    if n <= 2:
+        return n
+    elif n == 3:
+        return 4
+    # Set cookie conditions for n == 3
+    #
+    one_cookie = 4
+    two_cookie = 2
+    three_cookie = 1
+
+    for i in range(3, n):
+        current = one_cookie + two_cookie + three_cookie
+        three_cookie = two_cookie
+        two_cookie = one_cookie
+        one_cookie = current
+
+    return current
+
+
+print(eating_cookies_iterative(5))
 
 
 if __name__ == "__main__":
